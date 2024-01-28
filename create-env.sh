20c7134e27cfb81963f787601a00bb7e72c436c9
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-29 05:11:49 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,5 +1,5 @@
 
-ech
-POSTGRESQL_PASSWORD
-ELASTICSEARCH_PASSWORD
-MASTODON_ADMIN_PASSWORD
+echo "COMPOSE_PROJECT_NAME=suitecrm" > .env
+echo "POSTGRESQL_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
+echo "ELASTICSEARCH_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
+ech0 "MASTODON_ADMIN_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
