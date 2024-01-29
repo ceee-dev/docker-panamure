06a2f9651ccc568090ffbd61544301ec7b497c6b
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-29 09:06:02 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "COMPOSE_PROJECT_NAME=suitecrm" > .env
+echo "COMPOSE_PROJECT_NAME=panamure" > .env
 echo "MASTODON_WEB_DOMAIN=ngja.gov.sriankan.social" >> .env
 echo "POSTGRESQL_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
 echo "MINIO_ROOT_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env 
