875d1d5fc693ad91c13f6db2af1e79373fe09f57
C-EEE ORG <146799720+ceeedevops@users.noreply.github.com>
2024-01-29 09:04:43 +0530

    Update create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -2,5 +2,6 @@
 echo "COMPOSE_PROJECT_NAME=suitecrm" > .env
 echo "MASTODON_WEB_DOMAIN=ngja.gov.sriankan.social" >> .env
 echo "POSTGRESQL_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
+echo "MINIO_ROOT_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env 
 echo "ELASTICSEARCH_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
 echo "MASTODON_ADMIN_PASSWORD=$(cat /dev/urandom | strings | grep -Eoa "[a-zA-Z0-9@$%&_]*" | head -n 10 | tr -d '\n'| cut -c1-16)" >> .env
